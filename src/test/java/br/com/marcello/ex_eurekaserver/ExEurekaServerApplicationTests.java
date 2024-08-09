aaf5d225ee43f13aeb0f4abdd5add986ba48a7ee
Eduardo Marcello <eduardo.duarte@soulasalle.com.br>
2024-08-09 19:32:59 -0300

    Criação do servidor eureka.

---
+++ src/test/java/br/com/marcello/ex_eurekaserver/ExEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcello.ex_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
